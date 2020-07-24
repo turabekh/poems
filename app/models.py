afd4163f065a65028f72b9ba74d4e39c2c81f69b
turaboyh <turabekh@gmail.com>
2020-07-24 10:10:33 -0400

    logging and error handlers setup complete

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    image_file = db.Column(db.String(120), nullable=True, default="default.jpg")
+    image_file = db.Column(db.String(120), nullable=True)
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
