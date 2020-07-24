3d7b6e002b72330c48d8e7ddbfade901e8b4383c
turaboyh <turabekh@gmail.com>
2020-07-24 06:36:07 -0400

    login route and form, and wtforms set up

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, poems=poems)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"login requested for user: {form.username.data} and remember me: {form.remember_me.data}", "success")
+        return redirect(url_for("index"))
+    return render_template("login.html", form=form)
